1abc59b54f4a79cbfe00506dd6c937f25492f143
EmreGOKTAS14 <78740774+EmreGOKTAS14@users.noreply.github.com>
2021-03-17 17:26:35 +0300

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -124,7 +124,7 @@ namespace SpamPing
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(246, 49);
             this.button1.TabIndex = 6;
-            this.button1.Text = "Başlat";
+            this.button1.Text = "Saldırıyı Başlat";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
